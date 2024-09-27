b376423a76147cc882879e16778023ed12e81d90
ricodroid404 <ricodroid2024@gmail.com>
2024-09-27 11:22:09 +0900

    first-commit

---
+++ androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/screens/Screen.kt
@@ -0,0 +1,7 @@
+package com.petros.efthymiou.dailypulse.android.screens
+
+enum class Screens(val route: String) {
+    ARTICLES("reminders"),
+    SOURCES("sources"),
+    ABOUT_DEVICE("about-device")
+}
